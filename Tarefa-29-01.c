86f27b36d58d198265e5505b20a28deef1ceea6f
Luige <luige030301@gmail.com>
2025-02-04 19:29:29 -0300

    add comments

--- Tarefa-29-01.c
+++ Tarefa-29-01.c
@@ -2,23 +2,25 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
-#define RED_PIN 13
+#define RED_PIN 13 
 #define GREEN_PIN 11
 #define BLUE_PIN 12
 
 int i = 0;
 const uint32_t led_pins[] = {RED_PIN, BLUE_PIN, GREEN_PIN};
 
-void setup(){
+void setup(){   // Função para configurar os pinos de saída
     gpio_init(RED_PIN);
     gpio_set_dir(RED_PIN, GPIO_OUT);
+
     gpio_init(GREEN_PIN);
     gpio_set_dir(GREEN_PIN, GPIO_OUT);
+
     gpio_init(BLUE_PIN);
     gpio_set_dir(BLUE_PIN, GPIO_OUT);
 }
 
-bool repeating_timer_callback(struct repeating_timer *timer) {
+bool repeating_timer_callback(struct repeating_timer *timer) {  // Função callback para o alarme
     gpio_put(led_pins[i], 0);
     i = (i + 1) % 3;
     gpio_put(led_pins[i], 1);
@@ -33,7 +35,7 @@ int main()
     gpio_put(led_pins[i], 1);
 
     struct repeating_timer timer;
-    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer); // Configuração do alarme
 
     while (true) {
         sleep_ms(1000);
